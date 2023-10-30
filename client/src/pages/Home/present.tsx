add4ad6b6f6b0b5d543590166e5224d60a83927f
Matej Vrzala <vrzala.matej@gmail.com>
2023-10-30 14:47:41 +0100

    tags

--- client/src/pages/Home/present.tsx
+++ client/src/pages/Home/present.tsx
@@ -11,11 +11,31 @@ export function Homepage() {
 		<div class="page">
       <h2>Read the web.</h2>
       <p>We help you to remember and forget. </p>
+      <br />
 
-      <b>Currently working on: </b>
+      <b>Current priorities:</b>
       <ul>
-        <li>Private bookmarks</li>
-        <li>proof: 2 users see only their bookmarks</li>
+        <li>Add minimal registration</li>
+        <li>Fix login with real credentials</li>
+        <li>Extension authentication</li>
+        <li>Extension single tab</li>
+        <li>Extension bulk save and close (session)</li>
+        <li>Reader: store reading progress</li>
+        <li>Processor view: keep/forget/schedule</li>
+        <li>Simple tags support</li>
+      </ul>
+
+      <b>Overview: </b>
+      <ul>
+        <li>N: registered users</li>
+        <li>Z: stored bookmarks</li>
+      </ul>
+
+      <b>Workflow</b>
+      <ul>
+        <li>Store open tabs</li>
+        <li>Process and organize them</li>
+        <li>Schedule and read</li>
       </ul>
 		</div>
 	);
