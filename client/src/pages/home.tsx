2847d00bc320b22de98735f3c6c0ca1c0b48faaf
Matej V <vrzala.matej@gmail.com>
2024-01-25 13:50:09 +0100

    lang

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -5,12 +5,11 @@ export function Homepage() {
 	return (
 		<div class="reader">
 			<h2>Research Tools</h2>
+			<p>WIP: Read, Write, Organize, Publish</p>
 			<ul>
-				<li>We do what is needed.. <a href="https://docs.google.com/document/d/1NRkgrdSc7QpXe8jdnAh3ECr4-aBgmSVmHPECsp6C3yk/edit">Roadmap</a>. </li>
-				<li>TODO: fix reading xp</li>
-				<li>TODO: add write storage and listing</li>
-				<li>Currently working on Read and Write experiences.</li>
-				<li>Complete demo should handle read, write, organize and publish.</li>
+				<li>We do what is needed.. <a href="https://docs.google.com/document/d/1NRkgrdSc7QpXe8jdnAh3ECr4gaBgmSVmHPECsp6C3yk/edit" target="_blank">Roadmap</a>. </li>
+				<li>Trivial bookmarking and reader</li>
+				<li>text browser storage</li>
 				<li>Figure out trivial theming and configuration system.</li>
 			</ul>
 			<p></p>
--- client/src/pages/thesis.tsx
+++ client/src/pages/thesis.tsx
@@ -35,7 +35,7 @@ class Thesis extends Component {
 		return (
 			<div class="page">
 				<div class="block">
-					<h2>Research Zone - Open Document</h2>
+					<h2>Research</h2>
 					<p>Each thesis starts with a question. Think carefully about your research question before making a decision of working on your thesis. It will take time to develop your thinking and we recommend you to start by gathering <a href="/resources">Resources</a> and improving your <a href="/advent">Time Management</a>. </p>				
 					<input type="text" placeholder="research question" value={this.state.question} onInput={(e) => this.state.question = e.target.value}/>
 					<TextEditor ref={this.state.editor} />
@@ -47,7 +47,7 @@ class Thesis extends Component {
 					<ul>
 						<li>your stored entry</li>
 						<li>open another one</li>
-						<li>another one</li>
+						<li>a) browser memory b) n problem c) server memory</li>
 					</ul>
 				</div>
 			</div>
