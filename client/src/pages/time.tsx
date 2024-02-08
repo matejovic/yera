bf8452d08768d4ada80949b91ca9a8564a332a45
Matejovic <matej-linux@hotmail.com>
2024-02-08 09:21:36 +0100

    product work

--- client/src/pages/time.tsx
+++ client/src/pages/time.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "preact/hooks";
+import TextEditor from "../components/TextEditor.tsx";
+import { createRef } from "preact";
 
 
 export function Time() {
@@ -7,11 +9,13 @@ export function Time() {
   function timer(min) {
     t_end = Date.now() + min * 60000;
   }
+  
+  let editor = createRef();
 
   return (
     <div class="page">
       <div class="block">
-      <h2>Time</h2>
+      <h2>Time Management</h2>
         <ul>
           <li>minutes and hours (5min 15min 30min 45min 1h 90min 2h 4h 8h 12h)</li>
           <li>days (1, 2, 3, 4, 5, 6, 7)</li>
@@ -21,9 +25,10 @@ export function Time() {
         </ul>
       </div>
       <div class="block">
-        <span>Timebox</span> <br/>
-        <textarea /><br/>
-        <button onClick={timer(60)}>Start</button>
+        <span>1h</span><br/>
+        <button onClick={timer(60)}>Start</button><br/>
+        <span>Notes</span> <input type="checkbox" /> <br/>
+        <TextEditor ref={editor} />
       </div>
     </div>
   );
