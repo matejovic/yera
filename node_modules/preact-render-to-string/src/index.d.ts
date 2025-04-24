e812f4bb08f1a0d73bdcc443bb17cead95913825
Matoo125 <vrzala.matej@gmail.com>
2025-04-24 10:32:24 +0200

    remove node modules

--- node_modules/preact-render-to-string/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { VNode } from 'preact';
-
-export default function renderToString(vnode: VNode, context?: any): string;
-
-export function render(vnode: VNode, context?: any): string;
-export function renderToString(vnode: VNode, context?: any): string;
-export function renderToStaticMarkup(vnode: VNode, context?: any): string;
